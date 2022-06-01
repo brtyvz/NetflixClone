57dec9c64dd27fa09700d5e8aaee42f1108d8702
Berat Yavuz <beratttyavuzz@gmail.com>
2022-06-01 23:20:02 +0300

    Play and Download Button Added

---
+++ NetflixClone/Controllers/DownloadsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadsViewController.swift
+//  NetflixClone
+//
+//  Created by Berat Yavuz on 31.05.2022.
+//
+
+import UIKit
+
+class DownloadsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground    }
+    
+
+    
+
+}
---
+++ NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by Berat Yavuz on 31.05.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground    }
+    
+
+ 
+
+}
